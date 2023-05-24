640ad4cb6f745d50201aa39964263b834f1f0149
之愁 苍 <1594480261@qq.com>
2023-05-24 14:11:25 +0800

    添加项目文件。

---
+++ 将.xls转换为.xlsx/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Excel;
+
+namespace 将.xls转换为.xlsx
+{
+    [ExcelImporter(IsLabelingError = true, IsDisableAllFilter = true)]
+    public  class Class1
+    {
+
+        [ImporterHeader(Name = "ED_PDNo")]
+        public string a{get;set;}
+        [ImporterHeader(Name = "型号")]
+        public string b{get;set;}
+        [ImporterHeader(Name = "包装型态")]
+        public string g{get;set;}
+        [ImporterHeader(Name = "材料批号")]
+        public string c{get;set;}
+        [ImporterHeader(Name = "加工数量")]
+        public string d{get;set;}
+        [ImporterHeader(Name = "订单编码")]
+        public string f{get;set;}
+      
+    }
+}
---
+++ 将.xls转换为.xlsx/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Excel;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using 将.xls转换为.xlsx;
+using 将.xls转换为.xlsx.FixtureDataImportFromExcel.Common;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.IO;
+
+
+var xlsPath = @"C:\Users\Lenovo\Desktop\新建文件夹 (8)\产品资讯.xls";
+var   newExcelPath= @"C:\Users\Lenovo\Desktop\新建文件夹 (8)\产品资讯.xlsx";
+var oldWorkbook = new HSSFWorkbook(new FileStream(xlsPath, FileMode.Open));
+var oldWorkSheet = oldWorkbook.GetSheetAt(0);
+
+using (var fileStream = new FileStream(newExcelPath, FileMode.Create))
+{
+    var newWorkBook1 = new XSSFWorkbook();
+    var sheet = oldWorkSheet.CrossCloneSheet(newWorkBook1, "Sheet1");
+    newWorkBook1.Add(sheet);
+    newWorkBook1.Write(fileStream);
+    newWorkBook1.Close();
+
+}
+
+
+
+IImporter Importer = new ExcelImporter ();
+var import = await Importer.Import<Class1>(newExcelPath);
+
+
+//这是将IFormFile转换成HSSFWorkbook
+//public void ReadExcel(IFormFile file)
+//{
+//    using (var stream = file.OpenReadStream())
+//    {
+//        var workbook = new HSSFWorkbook(stream);
+//        var sheet = workbook.GetSheetAt(0);
+//        for (int i = sheet.FirstRowNum; i <= sheet.LastRowNum; i++)
+//        {
+//            var row = sheet.GetRow(i);
+//            if (row == null) continue;
+//            for (int j = row.FirstCellNum; j < row.LastCellNum; j++)
+//            {
+//                var cell = row.GetCell(j);
+//                if (cell == null) continue;
+//                var value = cell.ToString();
+//                // 处理单元格数据
+//            }
+//        }
+//    }
+//}
+Console.WriteLine("Hello, World!");
